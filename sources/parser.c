80bcfaf3476d072470a8cfb08b4f3e748a07eb4a
tlahin <teemu.lahin@gmail.com>
2022-10-05 10:11:02 +0300

    new parsers

--- sources/parser.c
+++ sources/parser.c
@@ -2,6 +2,22 @@
 
 #include "filler.h"
 
+int number_parser(char last)
+{
+    char    tmp;
+    int     nbr;
+
+    tmp = last;
+    nbr = 0;
+    while (ft_isdigit(tmp))
+    {
+        nbr = nbr * 10 + (tmp - '0');
+        if (read(0, &tmp,1) < 0)
+            return (-1);
+    }
+    return (nbr);
+}
+
 int size_parser(int *row, int *col)
 {
     char    tmp;
@@ -18,6 +34,32 @@ int size_parser(int *row, int *col)
     *col = number_parser(tmp);
     if (*row <= 0 || *col <= 0 || state == -1)
         return (-1);
+    return (0);
+}
+
+int board_parser(t_struct *data)
+{
+    char    tmp[1];
+    int     row;
+    int     col;
+
+    row = 0;
+    col = 0;
+    while (row < data->board_size.row)
+    {
+        while (col < data->board_size.col)
+        {
+            if (read(0, tmp, 1) == -1)
+                return (-1);
+            if (ft_strchr("XOxo.", tmp[0]))
+            {
+                data->board[row][col] = ft_toupper(tmp[0]); //char_parser(tmp[0]);
+                col++;
+            }
+        }
+        row++;
+    }
+    update_centers(data);
     return (0);
 }
 
